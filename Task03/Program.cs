a578e3d62152f6250b1b6a1889a09d66478d7f76
Anna <kochurovaav@gmail.com>
2023-02-12 11:35:36 +0200

    Task03 is done

---
+++ Task03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Программа, которая определяет, присутствует ли заданное число в массиве: 4; [6, 8, 4, 9]-> да.
+
+int [] array = GetArray(4, -8, 8);
+System.Console.WriteLine(String.Join(" ", array));
+
+
+int [] GetArray (int size, int minValue, int maxValue)
+{
+    int [] res = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        res [i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+int n = array.Length;
+int find = 4;
+int i = 0;
+
+while (i < n)
+{
+    if(array[i] == find)
+    {
+        System.Console.WriteLine($"{find} - > yes");
+    }
+    
+    i = i + 1;
+}
+
